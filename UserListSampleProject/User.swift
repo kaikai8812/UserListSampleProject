d794a1568bff2b4dec461d3499dfdc666dba90e9
kaikai <gamera1000x@gmail.com>
2023-11-06 00:28:11 +0900

    Initial Commit

---
+++ UserListSampleProject/User.swift
@@ -0,0 +1,6 @@
+//
+//  User.swift
+//  UserListSampleProject
+//
+//  Created by 青山凱 on 2023/11/06.
+//
---
+++ UserListSampleProject/UserListSampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserListSampleProjectApp.swift
+//  UserListSampleProject
+//
+//  Created by 青山凱 on 2023/11/06.
+//
+
+import SwiftUI
+
+@main
+struct UserListSampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
