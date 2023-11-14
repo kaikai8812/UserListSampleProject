98618c196fdafe23d77cadbe5ec99f1acc403f56
kaikai <gamera1000x@gmail.com>
2023-11-14 23:16:44 +0900

    StateObjectについて、少し勉強

---
+++ UserListSampleProject/sampleView.swift
@@ -0,0 +1,18 @@
+//
+//  sampleView.swift
+//  UserListSampleProject
+//
+//  Created by 青山凱 on 2023/11/14.
+//
+
+import SwiftUI
+
+struct sampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sampleView()
+}
